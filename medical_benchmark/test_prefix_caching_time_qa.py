b3c33f154c38e44e310b4133d9a51a9eb4d11f4b
Maxppddcsz <pzx118000@163.com>
2024-04-08 13:54:18 +0800

    qa dataset

---
+++ medical_benchmark/test_prefix_caching_time_qa.py
@@ -0,0 +1,354 @@
+"""Benchmark offline inference throughput."""
+import argparse
+import json
+import random
+import time
+from typing import List, Optional, Tuple
+from datasets import load_dataset
+import torch
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          PreTrainedTokenizerBase)
+from tqdm import tqdm
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+PROMPT = "You are a helpful assistant in recognizes the content of tables in markdown format. Here is a table as fellows. You need to answer my question about the table.\n# Table\n|Opening|Opening|Sl. No.|Film|Cast|Director|Music Director|Notes|\n|----|----|----|----|----|----|----|----|\n|J A N|9|1|Agni Pushpam|Jayabharathi, Kamalahasan|Jeassy|M. K. Arjunan||\n|J A N|16|2|Priyamvada|Mohan Sharma, Lakshmi, KPAC Lalitha|K. S. Sethumadhavan|V. Dakshinamoorthy||\n|...|...|...|...|...|...|...|...|...|\n# Question\nWhat s the content in the (1,1) cells"
+
+def flatten_list(nested_list):
+    flattened = []
+    for item in nested_list:
+        if isinstance(item, list):
+            # 如果是列表，递归调用函数进一步提取元素
+            flattened.extend(flatten_list(item))
+        elif isinstance(item, str):
+            # 如果是字符串，直接添加到结果列表
+            flattened.append(item)
+    return flattened
+
+def sample_requests(
+    num_requests: int,
+    tokenizer: PreTrainedTokenizerBase,
+    fixed_output_len: Optional[int],
+    attack_prompt_id: int = -1
+)-> Tuple[List[Tuple[str, int, int]], Tuple[str, int, int]]:
+    if fixed_output_len is not None and fixed_output_len < 4:
+        raise ValueError("output_len too small")
+
+    # Load the dataset.
+    # with open(dataset_path) as f:
+    #     dataset = json.load(f)
+    dataset = load_dataset('FreedomIntelligence/huatuo_encyclopedia_qa')
+    dataset = [(data['questions'][0], data['answers']) for data in dataset["test"]]
+
+
+    # Tokenize the prompts and completions.
+    prompts = [prompt for prompt, _ in dataset]
+    prompts = flatten_list(prompts)
+    # print(prompts)
+    prompt_token_ids = tokenizer(prompts).input_ids
+    completions = [completion for _, completion in dataset]
+    completions = flatten_list(completions)
+    completion_token_ids = tokenizer(completions).input_ids
+    tokenized_dataset = []
+    for i in range(len(dataset)):
+        output_len = len(completion_token_ids[i])
+        if fixed_output_len is not None:
+            output_len = fixed_output_len
+        #print(prompts[i])
+        tokenized_dataset.append((prompts[i], prompt_token_ids[i], output_len))
+
+    # Filter out too long sequences.
+    filtered_dataset: List[Tuple[str, int, int]] = []
+    for prompt, prompt_token_ids, output_len in tokenized_dataset:
+        prompt_len = len(prompt_token_ids)
+        if prompt_len < 4 or output_len < 4:
+            # Prune too short sequences.
+            continue
+        if prompt_len > 1024 or prompt_len + output_len > 2048:
+            # Prune too long sequences.
+            continue
+        filtered_dataset.append((prompt, prompt_len, output_len))
+
+    # Sample the requests.
+    # sampled_requests = random.sample(filtered_dataset, num_requests)
+    sampled_requests = filtered_dataset[:num_requests]
+    attack_prompt = filtered_dataset[attack_prompt_id]
+    return sampled_requests,attack_prompt
+
+
+def run_vllm(
+    requests: List[Tuple[str, int, int]],
+    attack_prompt: Tuple[str, int, int],
+    model: str,
+    tokenizer: str,
+    quantization: Optional[str],
+    tensor_parallel_size: int,
+    seed: int,
+    n: int,
+    use_beam_search: bool,
+    trust_remote_code: bool,
+    dtype: str,
+    max_model_len: Optional[int],
+    enforce_eager: bool,
+    kv_cache_dtype: str,
+    device: str,
+    enable_prefix_caching: bool,
+    gpu_memory_utilization: float = 0.9,
+) -> Tuple[float, float]:
+    from vllm import LLM, SamplingParams
+    llm = LLM(model=model,
+              tokenizer=tokenizer,
+              quantization=quantization,
+              tensor_parallel_size=tensor_parallel_size,
+              seed=seed,
+              trust_remote_code=trust_remote_code,
+              dtype=dtype,
+              max_model_len=max_model_len,
+              gpu_memory_utilization=gpu_memory_utilization,
+              enforce_eager=enforce_eager,
+              kv_cache_dtype=kv_cache_dtype,
+              device=device,
+              enable_prefix_caching=enable_prefix_caching)
+    
+    prompt,prompt_token_id,output_len = attack_prompt
+    sampling_params = SamplingParams(
+        n=n,
+        temperature=0.0,
+        top_p=1.0,
+        use_beam_search=use_beam_search,
+        ignore_eos=True,
+        max_tokens=100,
+    )
+    # start1 = time.time()
+    #     # FIXME(woosuk): Do not use internal method.
+    # llm._add_request(
+    #         prompt=prompt,
+    #         prompt_token_ids=None,
+    #         sampling_params=sampling_params,
+    # )
+    # end1 = time.time()
+    start = time.perf_counter()
+    # FIXME(woosuk): Do not use internal method.
+    output = llm.generate(
+        prompt,
+        sampling_params=sampling_params
+    )
+    #output = llm._run_engine(use_tqdm=True)
+    end = time.perf_counter()
+    attack_elapsed_time = end - start
+    print(f"attack1 {attack_elapsed_time} s, generate {output},{output[0].metrics.finished_time-output[0].metrics.first_token_time}")
+
+    # Add the requests to the engine.
+    sampling_params = SamplingParams(
+            n=n,
+            temperature=0.0,
+            top_p=1.0,
+            use_beam_search=use_beam_search,
+            ignore_eos=True,
+            max_tokens=100,
+        )
+        # FIX
+    prompts = [prompt for prompt,_,_ in requests]
+    # for prompt, _, output_len in requests:
+    #     sampling_params = SamplingParams(
+    #         n=n,
+    #         temperature=0.0,
+    #         top_p=1.0,
+    #         use_beam_search=use_beam_search,
+    #         ignore_eos=True,
+    #         #max_tokens=output_len,
+    #     )
+    #     # FIXME(woosuk): Do not use internal method.
+    #     llm._add_request(
+    #         prompt=prompt,
+    #         prompt_token_ids=None,
+    #         sampling_params=sampling_params,
+    #     )
+    start = time.perf_counter()
+    # FIXME(woosuk): Do not use internal method.
+    #llm._run_engine(use_tqdm=True)
+    llm.generate(
+        prompts,
+        sampling_params=sampling_params
+    )
+    end = time.perf_counter()
+    elapsed_time = end - start 
+
+    prompt,prompt_token_id,output_len = attack_prompt
+    # prompt = 
+    sampling_params = SamplingParams(
+        n=n,
+        temperature=0.0,
+        top_p=1.0,
+        use_beam_search=use_beam_search,
+        ignore_eos=True,
+        max_tokens=100,
+    )
+    # start1 = time.time()
+    #     # FIXME(woosuk): Do not use internal method.
+    # llm._add_request(
+    #         prompt=prompt,
+    #         prompt_token_ids=None,
+    #         sampling_params=sampling_params,
+    # )
+    # end1 = time.time()
+    start = time.perf_counter()
+    # FIXME(woosuk): Do not use internal method.
+    # output = llm._run_engine(use_tqdm=True)
+    output = llm.generate(
+        prompt,
+        sampling_params=sampling_params
+    )
+    end = time.perf_counter()
+    attack_elapsed_time = end - start
+    print(f"attack2 {attack_elapsed_time} s, generate {output},{output[0].metrics.finished_time-output[0].metrics.first_token_time}")
+
+    return elapsed_time,attack_elapsed_time
+
+def main(args: argparse.Namespace):
+    print(args)
+    random.seed(args.seed)
+
+    # Sample the requests.
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.tokenizer, trust_remote_code=args.trust_remote_code)
+        
+    requests,attack_prompt = sample_requests(args.num_prompts, tokenizer,
+                                   args.output_len,200)
+    print(attack_prompt)
+    if args.backend == "vllm":
+        elapsed_time,attack_elapsed_time = run_vllm(
+            requests, attack_prompt, args.model, args.tokenizer, args.quantization,
+            args.tensor_parallel_size, args.seed, args.n, args.use_beam_search,
+            args.trust_remote_code, args.dtype, args.max_model_len,
+            args.enforce_eager, args.kv_cache_dtype, args.device,
+            args.enable_prefix_caching, args.gpu_memory_utilization)
+    else:
+        raise ValueError(f"Unknown backend: {args.backend}")
+    
+    # total_num_tokens = sum(prompt_len + output_len
+    #                        for _, prompt_len, output_len in requests)
+    # print(f"Throughput: {len(requests) / elapsed_time:.2f} requests/s, "
+    #       f"{total_num_tokens / elapsed_time:.2f} tokens/s")
+    
+    # attack_prompt = sample_requests(args.dataset, 1, tokenizer,
+    #                                args.output_len, 1)
+    # if args.backend == "vllm":
+    #     attack_elapsed_time = run_vllm(
+    #         attack_prompt, args.model, args.tokenizer, args.quantization,
+    #         args.tensor_parallel_size, args.seed, args.n, args.use_beam_search,
+    #         args.trust_remote_code, args.dtype, args.max_model_len,
+    #         args.enforce_eager, args.kv_cache_dtype, args.device,
+    #         args.enable_prefix_caching, args.gpu_memory_utilization)
+    # else:
+    #     raise ValueError(f"Unknown backend: {args.backend}")
+    print(f"Different prompts: {elapsed_time} s, Attacker prompt: {attack_elapsed_time:.2f} s")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Benchmark the throughput.")
+    parser.add_argument("--backend",
+                        type=str,
+                        choices=["vllm", "hf", "mii"],
+                        default="vllm")
+    parser.add_argument("--input-len",
+                        type=int,
+                        default=None,
+                        help="Input prompt length for each request")
+    parser.add_argument("--output-len",
+                        type=int,
+                        default=None,
+                        help="Output length for each request. Overrides the "
+                        "output length from the dataset.")
+    parser.add_argument("--model", type=str, default="facebook/opt-125m")
+    parser.add_argument("--tokenizer", type=str, default=None)
+    parser.add_argument('--quantization',
+                        '-q',
+                        choices=['awq', 'gptq', 'squeezellm', None],
+                        default=None)
+    parser.add_argument("--tensor-parallel-size", "-tp", type=int, default=1)
+    parser.add_argument("--n",
+                        type=int,
+                        default=1,
+                        help="Number of generated sequences per prompt.")
+    parser.add_argument("--use-beam-search", action="store_true")
+    parser.add_argument("--num-prompts",
+                        type=int,
+                        default=1000,
+                        help="Number of prompts to process.")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--hf-max-batch-size",
+                        type=int,
+                        default=None,
+                        help="Maximum batch size for HF backend.")
+    parser.add_argument('--trust-remote-code',
+                        action='store_true',
+                        help='trust remote code from huggingface')
+    parser.add_argument(
+        '--max-model-len',
+        type=int,
+        default=None,
+        help='Maximum length of a sequence (including prompt and output). '
+        'If None, will be derived from the model.')
+    parser.add_argument(
+        '--dtype',
+        type=str,
+        default='auto',
+        choices=['auto', 'half', 'float16', 'bfloat16', 'float', 'float32'],
+        help='data type for model weights and activations. '
+        'The "auto" option will use FP16 precision '
+        'for FP32 and FP16 models, and BF16 precision '
+        'for BF16 models.')
+    parser.add_argument('--gpu-memory-utilization',
+                        type=float,
+                        default=0.9,
+                        help='the fraction of GPU memory to be used for '
+                        'the model executor, which can range from 0 to 1.'
+                        'If unspecified, will use the default value of 0.9.')
+    parser.add_argument("--enforce-eager",
+                        action="store_true",
+                        help="enforce eager execution")
+    parser.add_argument(
+        "--kv-cache-dtype",
+        type=str,
+        choices=["auto", "fp8_e5m2"],
+        default="auto",
+        help=
+        'Data type for kv cache storage. If "auto", will use model data type.')
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda",
+        choices=["cuda"],
+        help='device type for vLLM execution, supporting CUDA only currently.')
+    parser.add_argument(
+        "--enable-prefix-caching",
+        action='store_true',
+        help="enable automatic prefix caching for vLLM backend.")
+    args = parser.parse_args()
+    if args.tokenizer is None:
+        args.tokenizer = args.model
+
+    if args.backend == "vllm":
+        if args.hf_max_batch_size is not None:
+            raise ValueError("HF max batch size is only for HF backend.")
+    elif args.backend == "hf":
+        if args.hf_max_batch_size is None:
+            raise ValueError("HF max batch size is required for HF backend.")
+        if args.quantization is not None:
+            raise ValueError("Quantization is only for vLLM backend.")
+    elif args.backend == "mii":
+        if args.dtype != "auto":
+            raise ValueError("dtype must be auto for MII backend.")
+        if args.n != 1:
+            raise ValueError("n must be 1 for MII backend.")
+        if args.use_beam_search:
+            raise ValueError("Beam search is not supported for MII backend.")
+        if args.quantization is not None:
+            raise ValueError("Quantization is only for vLLM backend.")
+        if args.hf_max_batch_size is not None:
+            raise ValueError("HF max batch size is only for HF backend.")
+        if args.tokenizer != args.model:
+            raise ValueError("Tokenizer must be the same as the model for MII "
+                             "backend.")
+    main(args)
